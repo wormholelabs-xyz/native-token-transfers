5ef7f6a4e4f2fa4b4bf190ac52cc3b7625320446
Csongor Kiss <kiss.csongor.kiss@gmail.com>
2024-07-23 18:33:12 +0100

    evm: implement attestationReceived in abstract function
    this requires making `_verifyPeer` abstract

--- evm/src/interfaces/IManagerBase.sol
+++ evm/src/interfaces/IManagerBase.sol
@@ -100,6 +100,32 @@ interface IManagerBase {
     /// @param chainId The target Wormhole chain id
     error PeerNotRegistered(uint16 chainId);
 
+    /// @notice Called by a Transceiver contract to deliver a verified attestation.
+    /// @dev This function enforces attestation threshold and replay logic for messages. Once all
+    ///      validations are complete, this function calls `executeMsg` to execute the command specified
+    ///      by the message.
+    /// @param sourceChainId The Wormhole chain id of the sender.
+    /// @param sourceNttManagerAddress The address of the sender's NTT Manager contract.
+    /// @param payload The VAA payload.
+    function attestationReceived(
+        uint16 sourceChainId,
+        bytes32 sourceNttManagerAddress,
+        TransceiverStructs.NttManagerMessage memory payload
+    ) external;
+
+    /// @notice Called after a message has been sufficiently verified to execute
+    ///         the command in the message.
+    /// @dev This function is exposed as a fallback for when an `Transceiver` is deregistered
+    ///      when a message is in flight.
+    /// @param sourceChainId The Wormhole chain id of the sender.
+    /// @param sourceNttManagerAddress The address of the sender's nttManager contract.
+    /// @param message The message to execute.
+    function executeMsg(
+        uint16 sourceChainId,
+        bytes32 sourceNttManagerAddress,
+        TransceiverStructs.NttManagerMessage memory message
+    ) external;
+
     /// @notice Fetch the delivery price for a given recipient chain transfer.
     /// @param recipientChain The Wormhole chain ID of the transfer destination.
     /// @param transceiverInstructions The transceiver specific instructions for quoting and sending
